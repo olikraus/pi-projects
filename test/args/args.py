81e9ecca1b39e687b59d43979f94ad40ca826b0e
kraus <olikraus@gmail.com>
2021-04-25 23:27:31 +0200

    args

---
+++ test/args/args.py
@@ -0,0 +1,24 @@
+import argparse
+
+parser = argparse.ArgumentParser(description='test the args', 
+formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('-c',
+                    default='all',
+                    const='all',
+                    nargs='?',
+                    choices=['eject', 'sort', 'all'],
+                    help='''Define command to execute (default: %(default)s)
+    eject: Eject a card into sorter
+    sort: Move a cart from the sorter into a basket
+''')
+parser.add_argument('-n', 
+  action='store',
+  nargs='?', 
+  default=1,
+  const=1,
+  type=int,
+  help='repeat count')
+parser.print_help()
+args = parser.parse_args()
+print(args)
+print(args.n)
